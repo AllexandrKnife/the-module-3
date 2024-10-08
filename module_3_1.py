c02f6063ef83ff6030976dd09061425dbe75edbf
AllexandrKnife <kolchin.de@gmail.com>
2024-10-08 23:03:31 +0300

    module_3_1.py
    Вам необходимо написать 3 функции:
    Функция count_calls подсчитывающая вызовы остальных функций.
    Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
    Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
    Пункты задачи:
    Создать переменную calls = 0 вне функций.
    Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
    Создать функцию string_info с параметром string и реализовать логику работы по описанию.
    Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
    Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
    Вывести значение переменной calls на экран(в консоль).

---
+++ module_3_1.py
@@ -0,0 +1,29 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    length = len(string)
+    upper_case = string.upper()
+    lower_case = string.lower()
+    return (length, upper_case, lower_case)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    for item in list_to_search:
+        if string_lower == item.lower():
+            return True
+    return False
+
+
+# Примеры вызова функций
+print(string_info('Capybara'))  # (8, 'CAPYBARA', 'capybara')
+print(string_info('Armageddon'))  # (10, 'ARMAGEDDON', 'armageddon')
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # True
+print(is_contains('cycle', ['recycling', 'cyclic']))  # False
+
+print(calls)  # Выводим количество вызовов
